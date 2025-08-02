d1e19b6d0e7d9b7444ade8f0557086e948d19471
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-02 23:10:40 +0800

    Complete CH2: Nodes, L1: TextNode
    Gives a way to represent all the different types of inline text

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from textnode import TextNode, TextType
+
+def main():
+    print(TextNode("This is text", TextType.BOLD, "https://www.boot.dev"))
+main()
---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "link"
+    IMAGES = "image"
+
+class TextNode:
+    def __init__(self, TEXT, TEXT_TYPE, URL=None):
+        self.text = TEXT
+        self.text_type = TEXT_TYPE
+        self.url = URL
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
