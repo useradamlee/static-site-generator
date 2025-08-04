87d39465ab375d72f39dd02b8c6002a9b9ecfde9
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-04 15:22:07 +0800

    Add unit tests for LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -19,6 +19,12 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props=prop)
         result = node.props_to_html()
         self.assertEqual(result, '')
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    def test_leaf_to_html_href(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
 
 if __name__ == "__main__":
     unittest.main()
