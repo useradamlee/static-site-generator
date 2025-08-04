b94740681eaf10c5d44bd04fe479c454d7f4a295
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-04 15:21:49 +0800

    Complete CH2: Nodes, L4: LeafNode
    Gives to render HTML strings

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,13 @@ class HTMLNode:
         return " "+" ".join(HTML_attributes)
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    def to_html(self):
+        if not self.value:
+            raise ValueError("No value provided")
+        if not self.tag:
+            return str(self.value)
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
