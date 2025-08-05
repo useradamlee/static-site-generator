3975e90b20c6da2ad16f979665f34141f945313f
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-05 22:25:47 +0800

    Add unit tests for extract_markdown_images and extract_markdown_links

--- src/test_inline.py
+++ src/test_inline.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline import split_nodes_delimiter
+from inline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestInline(unittest.TestCase):
@@ -26,7 +26,6 @@ class TestInline(unittest.TestCase):
             ]
         )
 
-
     def test_delimiter_multiple(self):
         node = TextNode("`a` b `c`", TextType.TEXT)
         result = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -37,4 +36,32 @@ class TestInline(unittest.TestCase):
               TextNode(" b ", TextType.TEXT),
               TextNode("c", TextType.CODE),
             ]
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+    def test_extract_markdown_multiimages(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        , matches
+        )
+
+    def test_extract_markdown_multilinks(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev")
+            ],
+            matches
         )
