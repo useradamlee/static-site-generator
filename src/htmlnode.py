a1cc7c659a3a000a0744e235c57eede1d5957629
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-05 08:35:36 +0800

    Complete CH2: Nodes, L5: ParentNode
    Add a class which will handle the nesting of HTML nodes inside of one another.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,4 @@
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -31,3 +32,16 @@ class LeafNode(HTMLNode):
         if not self.tag:
             return str(self.value)
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("No tag provided")
+        if not self.children:
+            raise ValueError("Missing children")
+        content = ""
+        for child in self.children:
+            content += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{content}</{self.tag}>"
