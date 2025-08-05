2a139fa0723966e8210aedc777f3930081c20116
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-05 22:24:57 +0800

    Complete CH3: Inline, L4: Extract Links
    Gives the ability to extract the links and images from Markdown using regex

--- src/inline.py
+++ src/inline.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -17,3 +18,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     else:
                         new_nodes.append(TextNode(curr_val, text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
