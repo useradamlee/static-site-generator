fb3f2760c247e055f08c055e2106e9ef0d1af161
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-05 12:11:17 +0800

    Complete CH2: Nodes, L6: TextNode to HTMLNode
    Gives a way to convert a TextNode to an HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,4 @@
+from textnode import TextType
 
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
@@ -27,7 +28,7 @@ class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError("No value provided")
         if not self.tag:
             return str(self.value)
@@ -45,3 +46,18 @@ class ParentNode(HTMLNode):
         for child in self.children:
             content += child.to_html()
         return f"<{self.tag}{self.props_to_html()}>{content}</{self.tag}>"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINKS:
+        return LeafNode("a", text_node.text, props={"href": text_node.url})
+    if text_node.text_type == TextType.IMAGES:
+        return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
+    raise Exception("TextNode is none of the valid types")
