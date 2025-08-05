a971eccddda6151520eef5046dffeadd567a6c37
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-05 12:12:11 +0800

    Add unit tests for TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from htmlnode import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -24,7 +25,34 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
-
-
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.to_html(), "<b>This is a bold node</b>")
+    def test_italic(self):
+        node = TextNode("This is a italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.to_html(), "<i>This is a italic node</i>")
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINKS, URL="https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.to_html(), '<a href="https://www.google.com">This is a link node</a>')
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGES, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
 if __name__ == "__main__":
     unittest.main()
