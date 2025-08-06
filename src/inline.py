ba7fca65802501ef368c9b0bde9659cb2e1fd770
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-06 10:11:56 +0800

    Complete CH3: Inline, L4: Text to TextNodes
    Add a function that can convert a raw string of markdown text into a list of TextNode objects

--- src/inline.py
+++ src/inline.py
@@ -68,3 +68,21 @@ def split_nodes_link(old_nodes):
             split_nodes.append(TextNode(original_text, TextType.TEXT))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def text_to_textnodes(text):
+    return split_nodes_link(
+        split_nodes_image(
+            split_nodes_delimiter(
+                split_nodes_delimiter(
+                    split_nodes_delimiter(
+                        [TextNode(text, TextType.TEXT)],
+                        "**",
+                        TextType.BOLD),
+                    "_",
+                    TextType.ITALIC
+                ),
+                "`",
+                TextType.CODE
+            )
+        )
+    )
