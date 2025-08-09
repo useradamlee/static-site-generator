eba6ec8f7d4c091a98eff73a7c9a8b096ca8db6f
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-10 00:24:03 +0800

    Added extract_title function and unit tests for it
    It pulls a h1 header from a markdown file and return it

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import os
 import shutil
 
+def extract_title(markdown):
+    for line in markdown.split('\n'):
+        if line.startswith("# "):
+            return line.split("# ")[1].strip()
+    raise Exception("No h1 header found")
+
 def copystatic(source, destination):
     abs_dest = os.path.abspath(destination)
     abs_source = os.path.abspath(source)
---
+++ src/test_main.py
@@ -0,0 +1,26 @@
+import unittest
+
+from main import extract_title
+
+class TestExtractTitle(unittest.TestCase):
+    def test_simpleTitle(self):
+        md = """
+# Hello
+"""
+        title = extract_title(md)
+        self.assertEqual(title, "Hello")
+
+    def test_complexTitle(self):
+        md = """
+why is
+
+# Hello
+
+- not for uoy
+
+## nope
+
+### maybe?
+"""
+        title = extract_title(md)
+        self.assertEqual(title, "Hello")
