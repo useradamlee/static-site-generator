19571d46a2b29224bf241390fc4f09d61491926d
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-10 23:59:28 +0800

    Fix sys.argv issue
    It checked the wrong arguement

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from copystatic import copystatic
 from generatepage import generate_pages_recursive
 
 def main():
-    if not sys.argv[0]:
+    if not sys.argv[1]:
         basepath = '/'
     else:
-        basepath = sys.argv[0]
+        basepath = sys.argv[1]
     copystatic("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
