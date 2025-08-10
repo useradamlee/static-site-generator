8025371a03b4494a5078f483502796a101c82b8d
Adam <145332225+useradamlee@users.noreply.github.com>
2025-08-10 18:58:12 +0800

    Move copystatic function to newly created copystatic.py file

---
+++ src/copystatic.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+def copystatic(source, destination):
+    abs_dest = os.path.abspath(destination)
+    abs_source = os.path.abspath(source)
+    if os.path.exists(abs_dest):
+        print(f"Clearing contents of destination directory: {abs_dest}")
+        shutil.rmtree(abs_dest)
+    print(f"Adding back destination directory: {abs_dest}")
+    os.mkdir(abs_dest)
+    content = os.listdir(abs_source)
+    print(f"Contents of source directory: {content}")
+    for item in content:
+        item_path = os.path.join(abs_source, item)
+        if os.path.isfile(item_path):
+            print(f"File being copied: {item_path}")
+            shutil.copy(item_path, abs_dest)
+        else:
+            print(f"Directory being copied: {os.path.join(destination, item)}")
+            os.mkdir(os.path.join(destination, item))
+            copystatic(item_path, os.path.join(destination, item))
--- src/main.py
+++ src/main.py
@@ -1,31 +1,10 @@
-import os
-import shutil
+from copystatic import copystatic
 
 def extract_title(markdown):
     for line in markdown.split('\n'):
         if line.startswith("# "):
             return line.split("# ")[1].strip()
     raise Exception("No h1 header found")
-
-def copystatic(source, destination):
-    abs_dest = os.path.abspath(destination)
-    abs_source = os.path.abspath(source)
-    if os.path.exists(abs_dest):
-        print(f"Clearing contents of destination directory: {abs_dest}")
-        shutil.rmtree(abs_dest)
-    print(f"Adding back destination directory: {abs_dest}")
-    os.mkdir(abs_dest)
-    content = os.listdir(abs_source)
-    print(f"Contents of source directory: {content}")
-    for item in content:
-        item_path = os.path.join(abs_source, item)
-        if os.path.isfile(item_path):
-            print(f"File being copied: {item_path}")
-            shutil.copy(item_path, abs_dest)
-        else:
-            print(f"Directory being copied: {os.path.join(destination, item)}")
-            os.mkdir(os.path.join(destination, item))
-            copystatic(item_path, os.path.join(destination, item))
 
 def main():
     copystatic("static", "public")
